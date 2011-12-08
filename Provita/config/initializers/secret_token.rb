789b2047927db771b0a15e6e15f34d2cb034b122
Joshua Meier <joshim5@gmail.com>
2011-12-07 19:43:20 -0500

    Started a new Rails project

---
+++ Provita/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Provita::Application.config.secret_token = 'f633dc265dc4ce934653b0ee17586b3ba984971c069693571dc560e17dec17076488a47fa8cf8380d6231443a9db7231ac072b26bf37442c14c00a08546c182e'
